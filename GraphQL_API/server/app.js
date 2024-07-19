14d9a31cb3fa50ab7d1291a4049b06c5387b4b88
Matt Krozel <mattkrozel@Matts-MBP-2.attlocal.net>
2024-07-18 19:16:37 -0500

    task 6 push

--- GraphQL_API/server/app.js
+++ GraphQL_API/server/app.js
@@ -10,3 +10,14 @@ app.use('/graphql', graphqlHTTP({
 app.listen(4000,()=>{
   console.log('now listening for request on port 4000');
 });
+
+const connectionString = 'mongodb+srv://guest:guest@cluster0.wji5iwx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
+mongoose.connect(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.once('open', () => {
+  console.log('connected to database');
+});
